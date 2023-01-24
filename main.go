82c3b42bfbcf03335c05aa4e33819ec754ec66c6
Mohamed Abdifitah Ahmed <learntecnology30@gmail.com>
2023-01-24 19:09:41 +0300

    golang variables learning

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"golang/variables"
+)
+
+func main() {
+	variables.Variables()
+	fmt.Println("Hello World")
+}
---
+++ variables/variable.go
@@ -0,0 +1,11 @@
+package variables
+
+import (
+	"fmt"
+)
+
+func Variables() {
+	var string_var string = "string variable"
+	fmt.Printf("variable type type is and value is %v", string_var)
+
+}
