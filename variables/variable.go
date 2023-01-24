e2710265e5f4c93ec00fb190c41b41d4f88e2b3d
Mohamed Abdifitah Ahmed <learntecnology30@gmail.com>
2023-01-24 21:10:40 +0300

    variables created

--- variables/variable.go
+++ variables/variable.go
@@ -2,10 +2,23 @@ package variables
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func Variables() {
-	var string_var string = "string variable"
-	fmt.Printf("variable type type is and value is %v", string_var)
+	/* =========================== =====|| Variables initialize||================================ */
+	var stringVar string = "string variable"
+	var floatNum float32 = 46.6
+	var doubleNum float64 = 56.7
+	var intVar int = 444
+	var int64Var int = 5
+	/* =========================== =====|| Print to console all variables ||================================ */
+	fmt.Println(stringVar, floatNum, doubleNum, intVar, int64Var)
+	// /* ================================|| integer conver to string  ||================================ */
+	int2str := strconv.Itoa(int64Var)
+	fmt.Printf("variable type %v", int2str)
+	/* ================================|| Float to int ||================================ */
+	floa2int := int(floatNum)
+	fmt.Printf("variable type %v", floa2int)
 
 }
